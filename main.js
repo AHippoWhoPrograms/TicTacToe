30c63a844440adc6908f73746a067677f111b906
AHippoWhoPrograms <mjduboc@gmail.com>
2020-06-19 14:36:40 -0400

    Game works.

--- main.js
+++ main.js
@@ -1,59 +1,271 @@
-var Module = {
+var Module = (() => {
 
-    isPlayer1: true,
+    let isPlayer1 = true;
 
-    renderButtons: function(player1, player2)
+    let renderButtons = function(player1, player2)
     {
-        let imageX = document.createElement("img");
-        let imageO = document.createElement("img");
-        imageX.src = "./X.png";
-        imageO.src = "./O.png";
         for(let i = 0; i < 9; i++)
         {
-            let button = getElementById("" + i);
+            let button = document.getElementById("" + i);
             if(i == 0)
             {
-                button.addEventListener('click', function()
+                button.addEventListener('click', function(e)
                 {
-                    if(this.isPlayer1)
-                    {
-                        button.backgroundImage = "url(" + player1.img_src + ")";
+                    let button = e.srcElement;
+                   
+                    if(isPlayer1) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                    else button.style.backgroundImage = "url(" + player2.img_src + ")"; 
 
-                        if(document.getElementById("2").backgroundImage == "url(" + player1.img_src + ")" &&
-                           document.getElementById("1").backgroundImage == "url(" + player1.img_src + ")")
+                    if(document.getElementById("1").style.backgroundImage == button.style.backgroundImage &&
+                       document.getElementById("2").style.backgroundImage == button.style.backgroundImage)
+                       {
+                           console.log("Game is over!!!");
+                       }
+                    
+                    else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                    {
+                        console.log("Game is over!!!");
+                    }
+                
+                    if(document.getElementById("3").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("6").style.backgroundImage == button.style.backgroundImage)
+                    {
+                        console.log("Game is over!!!");
+                    }
+
+                    if(isPlayer1) isPlayer1 = false;
+                    else isPlayer1 = true;
+                });
+            }
+
+            else if(i == 1)
+            {
+                button.addEventListener('click', function(e){
+                    let button = e.srcElement;
+                   
+                    if(isPlayer1) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                    else button.style.backgroundImage = "url(" + player2.img_src + ")"; 
+
+                    if(document.getElementById("0").style.backgroundImage == button.style.backgroundImage &&
+                       document.getElementById("3").style.backgroundImage == button.style.backgroundImage)
+                       {
+                           console.log("Game is over!!!");
+                       }
+                    
+                    else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("7").style.backgroundImage == button.style.backgroundImage)
+                    {
+                        console.log("Game is over!!!");
+                    }
+
+                    if(isPlayer1) isPlayer1 = false;
+                    else isPlayer1 = true;
+            });
+            }
+            else if(i == 2)
+            {
+                button.addEventListener('click', function(e)
+                {
+                    let button = e.srcElement;
+                    if(isPlayer1) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                    else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                    if(document.getElementById("0").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("1").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+                    else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("6").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                       console.log(button.style.backgroundImage);
+                    }
+                    else if(document.getElementById("5").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+
+                    if(isPlayer1) isPlayer1 = false;
+                    else isPlayer1 = true;
+                });
+            }
+            else if(i == 3)
+            {
+                button.addEventListener('click', function(e)
+                {
+                    let button = e.srcElement;
+                    if(this.isPlayer1) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                    else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                    if(document.getElementById("0").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("6").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+                    else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("5").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+
+                    if(isPlayer1) isPlayer1 = false;
+                    else isPlayer1 = true;
+                });
+            }
+            else if(i == 4)
+            {
+                button.addEventListener('click', function(e)
+                {
+                    let button = e.srcElement;
+                    if(isPlayer1) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                    else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                    if(document.getElementById("0").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+                    else if(document.getElementById("1").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("7").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+                    else if(document.getElementById("3").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("5").style.backgroundImage == button.style.backgroundImage)
+                    {
+                       console.log("Game is over!!!");
+                    }
+                    else if(document.getElementById("2").style.backgroundImage == button.style.backgroundImage &&
+                    document.getElementById("6").style.backgroundImage == button.style.backgroundImage)
+                    {
+                        console.log("Game is over!!!");
+                    }
+
+                    if(isPlayer1) isPlayer1 = false;
+                    else isPlayer1 = true;
+                });
+                }
+
+                else if(i == 5)
+                {
+                    button.addEventListener('click', function(e)
+                    {
+                        //change this to button.style.backgroundImage.
+                        let button = e.srcElement;
+
+                        if(isPlayer1 == true) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                        else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                        if(document.getElementById("2").style.backgroundImage == button.style.backgroundImage &&
+                           document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                           {
+                               console.log("Game is over!!!");
+                           }
+                        else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                           document.getElementById("3").style.backgroundImage == button.style.backgroundImage)
                            {
                                console.log("Game is over!!!");
                            }
                         
-                        else if(document.getElementById("3").backgroundImage == "url" + player1.img_src + ")" &&
-                                document.getElementById("6").backgroundImage == "url" + player1.img_src + ")")
-                                {
-                                    console.log("Game is over!!!");
-                                }
-                        else if(document.getElementById("4").backGroundImage == "url" + player1.img_src + ")" &&
-                                document.getElementById("8").backGroundImage == "url" + player1.img_src + ")")
-                                {
-                                    console.log("Game is over!!!");
-                                }
-                    }
-                    else
-                    {
-                        button.backgroundImage = "url(" + player2.img_src + ")";
+                        if(isPlayer1) isPlayer1 = false;
+                        else isPlayer1 = true;
+                    });
+                }
 
-                        if(document.getElementById("" + (i + 1)).backgroundImage == "url(" + player2.img_src + ")" &&
-                           document.getElementById("" + (i + 2)).backgroundImage == "url(" + player2.img_src + ")")
+                else if(i == 6)
+                {
+                    button.addEventListener('click', function(e)
+                    {
+                        let button = e.srcElement;
+
+                        if(isPlayer1 == true) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                        else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                        if(document.getElementById("3").style.backgroundImage == button.style.backgroundImage &&
+                           document.getElementById("0").style.backgroundImage == button.style.backgroundImage)
                            {
                                console.log("Game is over!!!");
                            }
-                           //please finish this...
-                    }
-                    
-                    
-                });
+                        else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("2").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+                        else if(document.getElementById("7").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+
+                        if(isPlayer1) isPlayer1 = false;
+                        else isPlayer1 = true;
+                    });
+                }
+
+                else if(i == 7)
+                {
+                    button.addEventListener('click', function(e)
+                    {
+                        let button = e.srcElement;
+
+                        if(isPlayer1 == true) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                        else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                        if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                           document.getElementById("1").style.backgroundImage == button.style.backgroundImage)
+                           {
+                               console.log("Game is over!!!");
+                           }
+                        else if(document.getElementById("6").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("8").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+
+                        if(isPlayer1) isPlayer1 = false;
+                        else isPlayer1 = true;
+                    });
+                }
+
+                else if(i == 8)
+                {
+                    button.addEventListener('click', function(e)
+                    {
+                        let button = e.srcElement;
+
+                        if(isPlayer1 == true) button.style.backgroundImage = "url(" + player1.img_src + ")";
+                        else button.style.backgroundImage = "url(" + player2.img_src + ")";
+
+                        if(document.getElementById("5").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("2").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+                        else if(document.getElementById("4").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("0").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+                        else if(document.getElementById("7").style.backgroundImage == button.style.backgroundImage &&
+                        document.getElementById("6").style.backgroundImage == button.style.backgroundImage)
+                        {
+                            console.log("Game is over!!!");
+                        }
+                        if(isPlayer1) isPlayer1 = false;
+                        else isPlayer1 = true;
+                    });
+                }
+                //finish writing this code. 
+                //change this.isPlayer1 to just isPlayer1 and add checks for the other cases of i to check whether isPlayer 1 is tur or false.
+
             }
         }
-    }
-};
+    
+    return {renderButtons};
+});
 
 function Player(num, img_src)
 {
@@ -62,3 +274,5 @@ function Player(num, img_src)
 
 var player1 = Player(1, "./X.png");
 var player2 = Player(2, "./O.png");
+var func = Module();
+func.renderButtons(player1, player2);
